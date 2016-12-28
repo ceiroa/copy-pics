6200ec35c9ed5b00fc76bcb1617b1001dc9e56bb
Carlos Eiroa <ceiroa@salesforce.com>
2016-12-27 22:16:02 -0600

    Traversing directory structure all the way down. Taking parameters for source and destination.

--- PicsFromPhone.php
+++ PicsFromPhone.php
@@ -4,43 +4,52 @@ ini_set('display_errors', 1);
 error_reporting(-1);
 error_reporting(E_ALL);
 
+
 function copyPics($from, $to) {
 
+	echo 'Source directory: ' . $from . "\n";
+	echo 'Target directory: ' . $to . "\n";
+	
 	//get list of files and folders
 	$children = scandir($from);
-
+	
+	print_r($children);
+	
 	if(!empty($children)) {
 		foreach($children as $value) {
+		
+			echo '***Inspecting ' . $value . "\n";
 			
 			$isfile = is_file($from . '/' . $value);
-			$ispic = substr($value, -strlen('.jpg')) === '.jpg';
-			$isvideo1 = substr($value, -strlen('.mp4')) === '.mp4';
-			$isvideo2 = substr($value, -strlen('.3gp')) === '.3gp';
-
+			$ispic1 = strtolower(substr($value, -strlen('.jpg'))) === '.jpg';
+			$ispic2 = strtolower(substr($value, -strlen('.png'))) === '.png';
+			$ispic3 = strtolower(substr($value, -strlen('.png'))) === '.gif';
+			$ispic4 = strtolower(substr($value, -strlen('.jpg'))) === '.jpeg';
+			$isvideo1 = strtolower(substr($value, -strlen('.mp4'))) === '.mp4';
+			$isvideo2 = strtolower(substr($value, -strlen('.3gp'))) === '.3gp';
+			$isvideo3 = strtolower(substr($value, -strlen('.3gp'))) === '.mov';
+			
 			//if it's a picture or video file, copy it to target
-			if($isfile && ($ispic || $isvideo1 || $isvideo2)) {
-				echo 'Picture found: ' . $value . "\n";
+			if($isfile && ($ispic1 || $ispic2 || $ispic3 || $ispic4 || $isvideo1 || $isvideo2 || $isvideo3)) {
+				echo '***Picture/video found: ' . $value . "\n";
 				//rename file if file with same name exists in target
 				if(is_file($to . '/' . $value)) {
-					$value += '_';
+					$value = round(microtime(true) * 1000) . '_' . $value;
 				}
 				copy($from . '/' . $value, $to . '/' . $value);	
 			}
 			//if it's a folder, call current function
-			elseif(is_dir($value) && $value != '.' && $value != '..') {
-				echo '***Folder found: ' . $value . "\n";
-				copyPics($value);
+			elseif(is_dir($from . '/' . $value) && $value != '.' && $value != '..') {
+				echo '***Folder found: ' . $from . '/' . $value . "\n";
+				copyPics($from . '/' . $value, $to);
 			}
 		}
+	} else {
+		echo $from . ' directory is empty.';
 	}
 }
 
-$SourcePath1 = '/Volumes/Untitled/DCIM/Camera';
-$SourcePath2 = '/Volumes/NO NAME/DCIM/Camera';
-$SourcePath3 = '/Volumes/NO NAME/Picture';
+$from = $argv[1];
+$to = $argv[2];
 
-$to = '/Users/ceiroa/Desktop/MyDocs/Pics/AbuPicsAll';
-
-copyPics($SourcePath1, $to);
-copyPics($SourcePath2, $to);
-copyPics($SourcePath3, $to);
+copyPics($from, $to);
--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 copy-pics
 =========
 
-Copy pictures and videos from multiple directories (connected phone, for example) and put in all in one folder with flat structure
+Copy pictures and videos from a folder structure, and put it all in one folder with flat structure. This solves the problem of downloading all your pictures from your iPhone to your Mac. By default, the Photos application imports them into a folder structure under yourusername/Pictures/Photos Library.photoslibrary/, with one image per lowest level folder. (It's basically a tree, with the images as leafs.)
 
-Source directories and target are hardcoded right now (cuz I'm lazy)
+Unfortunately there is no free application that let's you do this in an easy way.
+
+First create target directory where you want to store pictures. Then navigate to directory containing script, and run script as follows:
+
+    php PicsFromPhone sourceDirectory targetDirectory
+
+For example:
+
+	cd /Users/ceiroa/Desktop
+	mkdir PhotosFromLibrary
+	cd /Users/ceiroa/copy-pics/
+	php PicsFromPhone.php '/Users/ceiroa/Pictures/Photos Library.photoslibrary/' '/Users/ceiroa/Desktop/PhotosFromLibrary/'
+	
+Of course the script needs to be executable, so you may have to do something like this before you can run it:
+
+	sudo chmod a+x PicsFromPhone.php
+    
